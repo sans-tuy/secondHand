f490419f60303d05da280a244de9be10e85ef93f
sans-tuy <anwarsanusisan@gmail.com>
2022-06-30 08:28:58 +0700

    add redux configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './config/Router/router';
+import {Provider} from 'react-redux';
+import {store} from './config/Redux/store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </Provider>
   );
 }
